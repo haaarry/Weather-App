1708f0fda1e2633be0110bbc5d421e00c3c2e1ce
Harry A’Court <harry.acourt@sky.uk>
2016-04-06 11:18:38 +0100

    added a search button to the action bar

--- app/src/main/java/com/sky/weather/FavouritesActivity.java
+++ app/src/main/java/com/sky/weather/FavouritesActivity.java
@@ -2,6 +2,8 @@ package com.sky.weather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Toast;
 
 public class FavouritesActivity extends AppCompatActivity implements WeatherApiResponse{
@@ -22,5 +24,12 @@ public class FavouritesActivity extends AppCompatActivity implements WeatherApiR
     @Override
     public void returnedData(String output) {
         Toast.makeText(this, output, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
     }
 }
